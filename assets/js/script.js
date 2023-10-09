566aac907168a2c29ddaab206d37eb454b16871e
Stark <mattstark08@gmail.com>
2023-10-09 18:54:48 -0500

    clock counting down from zero with start click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,62 @@
 var startButton = document.querySelector(".start-button");
 var timerCount = document.querySelector(".timer-count");
-var questionArea = document.querySelector(".questions");
-var timersec = 60;
-var currentQuestionIndex = 0;
-var winner;
+var questionArea = document.querySelector(".question-area");
 
-var questions = {
+var timersec = 0;
+// var currentQuestionIndex = 0;
 
-}
+// var questions = {
+
+// }
 
 function start() {
+    timersec.textContent = timersec;
     var timeInterval = setInterval(function(){
+        //is this setting the timer?
         timersec--;
         timerCount.textContent = timersec;
         if( timer === 0){
             clearInterval(timeInterval)
             // determain if the user won or lost
         }
-    }, 10000);
+        // is this setting the speed of time interval? or length of time on timer???
+    }, 1000);
     displayquestion();
 }
 
-function displayquestion(){
+// function displayquestion(){
+//     var currentQuestion = questions[currentQuestionIndex];
+//     var pTag = document.createElement("p");
+//     pTag.textContent = currentQuestion.question;
+//     questionArea.appendChild(pTag);
+
+//     for( var i = 0; i < currentQuestion.answer.length; i++){
+//         var answer = currentQuestion.answer[i];
+//         var btn = document.createElement("button");
+//         questionArea.appendChild(btn);
+//     };
+
+// };
 
 
+// function answerIsCorrect(){
 
+// };
 
+// function answerIsWrong(){
 
+// };
 
+// questionArea.addEventListener("click", function(event){
+//     var currentQuestion =questions[currentQuestionIndex];
+//     if(event.target.matches("button")){
+//         answerIsCorrect();
+//     } else {
+//         answerIsWrong();
+//     }
+//     currentQuestionIndex++;
+//     displayquestion();
+// });
 
-    
-
-}
-
-
-
-
-function answerIsCorrect(){
-
-}
-
-function answerIsWrong(){
-
-}
 
 startButton.addEventListener("click", start);
