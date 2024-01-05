fb591261e36e817b3052956bbbc92a7daba06026
Stark <mattstark08@gmail.com>
2024-01-05 00:25:13 -0600

    core quiz styling functional

--- README.md
+++ README.md
@@ -4,9 +4,12 @@
 This quiz is an excercise in generating dynamic elements inside of html using javascript, as well as use of localStorage using JSON.
 
 ## Screen Shot
-![screenshot](./assets/images/Screenshotquiz.png)
+![originalScreenShot](./assets/images/Screenshotquiz.png)
 
 ## Links
 
 [GitHub]https://github.com/Matt0Stark/question_quiz/
 [GitHubPage]https://matt0stark.github.io/question_quiz/
+
+## Reflections
+I was proud of this quiz. Coming into coding with very little previous experience made for an uphill battle in the first couple weeks of the bootcamp, with javascript seeming especially daunting. The assignment was submitted without a style sheet, so was original was functional but boring question wise, and abjectly ugly to look at. It can be seen in its glory in the OriginalScreenShot, which I will proudly display here on my intetnet fridge as a reminder of my progress. 
--- assets/js/script.js
+++ assets/js/script.js
@@ -7,35 +7,30 @@ var timersec = 30;
 var currentQuestionIndex = 0;
 var quizScoreCounter = 0;
 
-RenderScoreboard();
+renderScoreboard();
 
 //done
-var questions = [
-    {
-        question: "what is 2+2",
-        answer: ["one","two","three","four"],
-        correct: "four",
-    },
-    {
-        question: "what is 2+3",
-        answer: ["three","four","five","six"],
-        correct: "five",
-    },
-    {
-        question: "what is 2+6",
-        answer: ["two","eight","taco","nine"],
-        correct: "eight",
-    },
-    {
-        question: "what is 2+1",
-        answer: ["one","three","nine","seven"],
-        correct: "three",
-    },
-    {
-        question: "what is 2+5",
-        answer: ["seven","eight","three","six"],
-        correct: "seven",
-    },
+var questions = [{
+    question: "what is 2+2",
+    answer: ["one", "two", "three", "four"],
+    correct: "four",
+}, {
+    question: "what is 2+3",
+    answer: ["three", "four", "five", "six"],
+    correct: "five",
+}, {
+    question: "what is 2+6",
+    answer: ["two", "eight", "taco", "nine"],
+    correct: "eight",
+}, {
+    question: "what is 2+1",
+    answer: ["one", "three", "nine", "seven"],
+    correct: "three",
+}, {
+    question: "what is 2+5",
+    answer: ["seven", "eight", "three", "six"],
+    correct: "seven",
+},
 ]
 
 var playerStorage = JSON.parse(localStorage.getItem("playerStorage")) || [];
@@ -48,7 +43,7 @@ function start() {
         if (timersec === 0) {
             clearInterval(timeInterval);
             resetScreen();
-        } else  if (currentQuestionIndex === 5){
+        } else if (currentQuestionIndex === 5) {
             clearInterval(timeInterval);
         };
 
@@ -57,14 +52,14 @@ function start() {
 };
 
 //does this even need to be here as its own function?
-function quiz(){
-    displayquestion();
+function quiz() {
+    displayQuestion();
 };
 
 
 //done?
-function displayquestion(){
-    
+function displayQuestion() {
+
     startButton.setAttribute("style", "display: none");
     resetScreen();
     var currentQuestion = questions[currentQuestionIndex];
@@ -72,7 +67,7 @@ function displayquestion(){
     pTag.textContent = currentQuestion.question;
     questionArea.appendChild(pTag);
 
-    for( var i = 0; i < currentQuestion.answer.length; i++){
+    for (var i = 0; i < currentQuestion.answer.length; i++) {
         var answer = currentQuestion.answer[i];
         var btn = document.createElement("button");
         console.log(btn);
@@ -81,59 +76,53 @@ function displayquestion(){
         questionArea.appendChild(btn);
         console.log(btn);
     };
-    
-       
+
+
 };
 
 //done
-function answerIsCorrect(){
-quizScoreCounter++;
-scoreCount.textContent = quizScoreCounter;
+function answerIsCorrect() {
+    quizScoreCounter++;
+    scoreCount.textContent = quizScoreCounter;
 };
 
 //done
-function answerIsWrong(){
+function answerIsWrong() {
     timersec -= 5;
 };
 
 // done?
-function endGame(){
+function endGame() {
     console.log("we made it to end of game");
-    var initials = prompt("Please enter your initials","JB");
+    var initials = prompt("Please enter your initials", "JB");
     console.log(initials);
-    var playerSubmission = {name: initials, score: quizScoreCounter};
+    var playerSubmission = { name: initials, score: quizScoreCounter };
     // push object to array
     playerStorage.push(playerSubmission);
     console.log(playerStorage);
- 
+
     localStorage.setItem("playerStorage", JSON.stringify(playerStorage));
-    RenderScoreboard();
-  //store user initials input and quizScoreCounter in local storage
+    renderScoreboard();
+    //store user initials input and quizScoreCounter in local storage
 };
 
 //done
-function resetScreen(){
-    questionArea.innerHTML="";
+function resetScreen() {
+    questionArea.innerHTML = "";
 };
 
 // will need to pull the obs, parse them, and put them somewhere on the page
-function RenderScoreboard(){
+function renderScoreboard() {
     var scoreBoard = JSON.parse(localStorage.getItem("playerStorage"));
     console.log("am I rendering?");
     console.log(scoreBoard);
     var scoreList = document.querySelector(".saved-player-scores");
-
-
     if (scoreBoard) {
-
-        for(i = 0; i < scoreBoard.length; i++){
-        
+        for (i = 0; i < scoreBoard.length; i++) {
             var li = document.createElement("li");
             li.textContent = scoreBoard[i].name + scoreBoard[i].score;
             scoreList.appendChild(li);
-
         };
-
     };
 
 
@@ -145,24 +134,22 @@ function RenderScoreboard(){
 };
 
 //done?
-questionArea.addEventListener("click", function(event){
+questionArea.addEventListener("click", function (event) {
     var currentQuestion = questions[currentQuestionIndex];
-    if(event.target.matches("button")){
-        if (event.target.id === currentQuestion.correct){
+    if (event.target.matches("button")) {
+        if (event.target.id === currentQuestion.correct) {
             answerIsCorrect();
         } else {
             answerIsWrong();
         }
         currentQuestionIndex++;
-        if (currentQuestionIndex === 5){
-            questionArea.innerHTML="";
+        if (currentQuestionIndex === 5) {
+            questionArea.innerHTML = "";
             endGame();
-        }else{
-            displayquestion();
-        }
-        
-        
-    }
+        } else {
+            displayQuestion();
+        };
+    };
 });
 
 //done
